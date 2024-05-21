2177cdaa06e34d2a46b5f8d8ed4b46cef418025b
vishnu anil <vishnuanil216@gmail.com>
2024-05-21 11:57:40 +0530

    responsive  website started

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+function button(){
+    <ul class="nav-links">
+<li class="home"><a href="#"></a>Home</li>
+<li><a href="#"></a>Tours</li>
+<li><a href="#"></a>Explore</li>
+<li><a href="#"></a>About</li>
+<li><a href="#footer"></a>Contact</li>
+</ul>
+
+}
